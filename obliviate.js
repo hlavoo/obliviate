4854316cca8d065f0d4fc8d6bcec2ca4c40645e6
henrylavoo <henrylavoo@gmail.com>
2017-06-15 17:19:06 -0400

    initial commit

---
+++ obliviate.js
@@ -0,0 +1,53 @@
+(function () {
+    var obliviate = false,
+        obliviating = false,
+        oneWeek = 7 * 24 * 60 * 60 * 1000,
+        lastWeek,
+        now;
+
+    function obliviateCache () {
+        if (obliviating === false) {
+            if (typeof chrome.browsingData !== 'undefined') {
+                obliviating = true;
+                now = new Date().getTime();
+                lastWeek = now - oneWeek;
+
+                chrome.browsingData.removeCache({
+                    'since': lastWeek
+                }, function() {
+                    obliviating = false;
+                });
+            }
+        }
+    }
+
+    function iconClick () {
+        obliviate = !obliviate;
+        if (obliviate) {
+            chrome.browserAction.setIcon({
+              path: 'icon-active.png'
+            });
+
+            chrome.browserAction.setTitle({
+              title: 'Obliviate active'
+            });
+
+            chrome.webRequest.onBeforeRequest.addListener(obliviateCache, {
+                urls: ["<all_urls>"]
+            });
+        } else {
+            chrome.browserAction.setIcon({
+                path: 'icon.png'
+            });
+
+            chrome.browserAction.setTitle({
+              title: 'Obliviate disabled'
+            });
+
+            chrome.webRequest.onBeforeRequest.removeListener(obliviateCache);
+        }
+    }
+
+    chrome.browserAction.onClicked.addListener(iconClick);
+}());
+
